8fd83532e16705ed9dee7a8eeab1737b6f5bd517
babobungmin <babobungmin@naver.com>
2019-04-27 16:22:55 +0900

    222

--- app/src/main/java/com/hh/a20190427_androidpractice/adapters/PizzaStoreAdapters.java
+++ app/src/main/java/com/hh/a20190427_androidpractice/adapters/PizzaStoreAdapters.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.hh.a20190427_androidpractice.R;
 import com.hh.a20190427_androidpractice.datas.PizzaStore;
 
@@ -34,25 +35,17 @@ public class PizzaStoreAdapters extends ArrayAdapter<PizzaStore> {
         View row = convertView;
 
         if(row == null){
-            row = inf.inflate(R.layout.pizza_spinner_list_item, null);
+            row = inf.inflate(R.layout.pizza_spinner_item, null);
         }
 
         PizzaStore storeData = mList.get(position);
 
-        ImageView logoImageView = row.findViewById(R.id.logoImg);
+        ImageView logoImageView = row.findViewById(R.id.logoImgView);
         TextView nameTxt = row.findViewById(R.id.nameTxt);
-        TextView locationTxt = row.findViewById(R.id.locationTxt);
-        TextView openTimeTxt = row.findViewById(R.id.openTimeTxt);
 
 
+        Glide.with(mContext).load(storeData.imageURL).into(logoImageView);
         nameTxt.setText(storeData.storeName);
-
-        locationTxt.setText(String.format("(%s)",storeData.location));
-        openTimeTxt.setText(storeData.openTime);
-
-
-
-
 
         return row;
     }
@@ -73,6 +66,9 @@ public class PizzaStoreAdapters extends ArrayAdapter<PizzaStore> {
         TextView nameTxt = row.findViewById(R.id.nameTxt);
         TextView locationTxt = row.findViewById(R.id.locationTxt);
         TextView openTimeTxt = row.findViewById(R.id.openTimeTxt);
+
+
+       // Glide.with(mContext).load(storeData.imageURL).into(logoImageView);
 
 
         nameTxt.setText(storeData.storeName);
